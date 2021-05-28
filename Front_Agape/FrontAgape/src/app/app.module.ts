fa7bfb062619288cfb37c9d734b057d976df36e2
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2021-05-27 21:24:09 -0300

    criando a pagina Home

--- Front_Agape/FrontAgape/src/app/app.module.ts
+++ Front_Agape/FrontAgape/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { NavebarComponent } from './navebar/navebar.component';
+import { TesteComponent } from './testeDIST/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    FooterComponent,
+    BannerComponent,
+    NavebarComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule
