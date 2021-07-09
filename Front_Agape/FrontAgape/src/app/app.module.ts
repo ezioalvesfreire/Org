6e26d0b318ef99382472f21db132cd6bca490672
ezioalvesfreire <53498926+ezioalvesfreire@users.noreply.github.com>
2021-07-09 12:50:23 -0300

    iniciando conexão com back-end

--- Front_Agape/FrontAgape/src/app/app.module.ts
+++ Front_Agape/FrontAgape/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import{ ROUTES } from './app.routes'
 
+import { AppService } from './app.service';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
@@ -17,6 +21,7 @@ import { HelpMeComponent } from './help-me/help-me.component';
 import { MediaPlayerComponent } from './media-player/media-player.component';
 import { BannerActiveComponent } from './banner-active/banner-active.component';
 import { LogoAgapeComponent } from './logo-agape/logo-agape.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
 
 @NgModule({
   declarations: [
@@ -32,13 +37,16 @@ import { LogoAgapeComponent } from './logo-agape/logo-agape.component';
     HelpMeComponent,
     MediaPlayerComponent,
     BannerActiveComponent,
-    LogoAgapeComponent
+    LogoAgapeComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Front_Agape/FrontAgape/src/app/form-register/form-register.component.ts
@@ -0,0 +1,47 @@
+/*import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-register',
+  templateUrl: './form-register.component.html',
+  styleUrls: ['./form-register.component.css']
+})
+export class FormRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}*/
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { AppService } from './app.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  contatos: Array<any>;
+  contato: any;
+
+  constructor(private service: AppService) {}
+
+  ngOnInit() {
+    this.contato = {};
+
+    this.service.listar()
+      .subscribe(resposta => this.contatos = resposta);
+  }
+
+  criar(frm: FormGroup) {
+    this.service.criar(this.contato).subscribe(resposta => {
+      this.contatos.push(resposta);
+
+      frm.reset();
+    });
+  }
+}
